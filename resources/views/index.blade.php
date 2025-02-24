458f5230094ef91029b11c5d94e4786f15646831
ainhoablanco <ablancoe2324@politecnics.barcelona>
2025-02-24 18:49:15 +0100

    add laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.principal')
+
+{{-- @section('titulo')
+    Indice
+@endsection --}}
+
+@section('titulo', 'Indice')
+
+@section('contenido')
+    <h1>Título</h1>
+    <p>Esta es la página índice</p>
+@endsection
