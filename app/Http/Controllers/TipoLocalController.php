70edb2afd500fc9b172b3c7056d0e1afa1dfd3ca
ainhoablanco <ablancoe2324@politecnics.barcelona>
2025-02-28 19:46:26 +0100

    add vendor

---
+++ app/Http/Controllers/TipoLocalController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TipoLocal;
+use Illuminate\Http\Request;
+
+class TipoLocalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TipoLocal $tipoLocal)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TipoLocal $tipoLocal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TipoLocal $tipoLocal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TipoLocal $tipoLocal)
+    {
+        //
+    }
+}
