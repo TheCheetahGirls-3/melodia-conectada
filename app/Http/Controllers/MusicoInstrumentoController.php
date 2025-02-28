7382728c4f866581e9420260c118ecfb0e565568
VpogF <vpoglonig2324@politecnics.barcelona>
2025-02-28 19:56:19 +0100

    Make Models
    Creación de modelos

---
+++ app/Http/Controllers/MusicoInstrumentoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MusicoInstrumento;
+use Illuminate\Http\Request;
+
+class MusicoInstrumentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MusicoInstrumento $musicoInstrumento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MusicoInstrumento $musicoInstrumento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MusicoInstrumento $musicoInstrumento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MusicoInstrumento $musicoInstrumento)
+    {
+        //
+    }
+}
