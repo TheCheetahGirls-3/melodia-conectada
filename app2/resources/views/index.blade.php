db88ba014835cec9efbe1d07766736637c4139f5
VpogF <vpoglonig2324@politecnics.barcelona>
2025-03-27 19:00:10 +0100

    css

--- app2/resources/views/index.blade.php
+++ app2/resources/views/index.blade.php
@@ -1,14 +1,9 @@
 @extends('layouts.inicio')
 
 @section('listado')
-
-
-
-=======
-<div id="app">
-    <listado :usuario="{{ json_encode(Auth::user()) }}"></listado>
-
-</div>
+    <div id="app">
+        <listado :usuario="{{ json_encode(Auth::user()) }}"></listado>
+    </div>
 @endsection
 
 @section('mapa')
