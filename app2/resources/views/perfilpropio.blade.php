c625f5d78dc3d6f38fdf291f553d556177b8d690
Natalia Calvo <ncalvom2324@politecnics.barcelona>
2025-04-30 19:28:40 +0200

    añado .env

--- app2/resources/views/perfilpropio.blade.php
+++ app2/resources/views/perfilpropio.blade.php
@@ -6,8 +6,9 @@
 
 @section('contenido')
 <div class="container">
-    <!-- Pasar el ID del usuario autenticado -->
+
     <perfil :id="{{ Auth::id() }}" :usuario-autenticado-id="{{ Auth::id() }}"></perfil>
+
 </div>
 
 @endsection
