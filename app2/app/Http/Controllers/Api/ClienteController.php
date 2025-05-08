8bc8fd3727c42713959421b6e52c20eabfa7bdbb
ainhoablanco <ablancoe2324@politecnics.barcelona>
2025-05-08 15:40:40 +0200

    prueba

--- app2/app/Http/Controllers/Api/ClienteController.php
+++ app2/app/Http/Controllers/Api/ClienteController.php
@@ -10,8 +10,9 @@ use Illuminate\Http\Request;
 class ClienteController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource..
      */
+
     public function index()
     {
         $clientes = Cliente::with('usuario', 'musicos.instrumentos','musicos.generos', 'locales.tipo_local')->get();
